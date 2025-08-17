47e863a72e9810deac33394a8159762997005f71
aaunmalik7 <aaunmalik7@gmail.com>
2025-08-17 23:33:51 +0500

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        flpGreen: "#0c7a43"
+      }
+    },
+  },
+  plugins: [],
+}
